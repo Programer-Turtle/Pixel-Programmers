5c336945771fe7852877e94400c5425a3dd87f1c
programer turtle <karsonulerick@gmail.com>
2025-11-12 23:38:13 -0500

    Updated

--- Scripts/UI.js
+++ Scripts/UI.js
@@ -1,12 +1,12 @@
 function LoadNavbar() {
   const Nav = document.createElement("nav");
-  Nav.innerHTML = `<a href="index.html" style="text-decoration: none; color: white;"><img style="width: 5rem; margin:4px;" src="https://cdn.pixabay.com/photo/2016/01/03/11/24/gear-1119298_1280.png" alt="Home"></a>`;
+  Nav.innerHTML = `<a href="index.html" style="text-decoration: none; color: white;"><img style="width: 5rem; margin:4px;" src="Images/Logo.PNG" alt="Home"></a>`;
   document.body.prepend(Nav);
 }
 
 function LoadFooter() {
   const Footer = document.createElement("footer");
-  Footer.innerHTML = `<div style="display: flex; justify-content: space-between; padding: 1rem"><img style="width: 5rem"src="https://cdn.pixabay.com/photo/2016/01/03/11/24/gear-1119298_1280.png"alt="logo"/><div style="display: flex"><a href=""><img style="width: 3rem; margin: 12px"src="Images/Facebook_Logo_Primary.png"alt="Facebook Logo"/></a></div></div>`;
+  Footer.innerHTML = `<div style="display: flex; justify-content: space-between; padding: 1rem"><img style="width: 5rem"src="Images/Logo.PNG"alt="logo"/><div style="display: flex"><a href="https://www.facebook.com/PixelProgrammers/"><img style="width: 3rem; margin: 12px"src="Images/Facebook_Logo_Primary.png"alt="Facebook Logo"/></a></div></div>`;
   document.body.appendChild(Footer);
 }
 
